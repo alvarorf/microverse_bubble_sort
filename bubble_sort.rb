0546c128e4999463533929a64e010be82f65d4f5
alvaro <aaruizf@unal.edu.co>
2020-09-07 17:06:37 -0500

    Completed first version of bubble_sort method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,33 +1,58 @@
-# Loop through an array of elements 
+=begin
+arr=[10,3,2,5,4]
+Loop through an array of elements (arr)
+First check if arr is already sorted
+If it is not sorted, then 
+First pass: Iterate through the length of arr
+We compare arr[i] with arr[i+1], so that if arr[i]>arr[i+1], 
+we switch them so we store arr[i] in a temporary variable temp=arr[i], 
+and then temp=arr[i]. Then arr[i]=arr[i+1], then arr[i+1]=temp
+Second pass: 
+First check if arr is already sorted
+Then, we iterate through length-1 of arr
+And so on...
+=end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def bubble_sort(argument)
-  len=argument.length
-  i=0
-  while i<len
-    if argument[i]>argument[i+1]
-      a=argument[i]
-      argument[i]=argument[i+1]
-      argument[i+1]=a
+def bubble_sort(arr)
+  
+  j=0
+  while j<arr.length-1
+    puts "J is #{j}"
+    puts "arr.length is #{arr.length}"
+    i=0
+    while i < arr.length-1
+      puts "i is #{i}"
+      puts "arr.length is #{arr.length}"
+      if arr[i]>arr[i+1]
+        temp = arr[i]
+        arr[i] = arr[i+1]
+        arr[i+1] = temp
+      end
       i+=1
+      print arr
     end
-    
+    j+=1
   end
-  return argument
+
+
+  return arr
+
+  
 end
 
-puts bubble_sort([6,2,5])
+test_arr = [26,6,4,3,1]
+puts bubble_sort(test_arr)
+puts test_arr
+
+
+
+
+
+
+
+
+
+
+
+
+
