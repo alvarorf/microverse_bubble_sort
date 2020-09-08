03859ce44ef4d7ee07fdcde7c816d0523e651267
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-09-08 21:10:35 +0200

    optimize the bubble_sort method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -10,33 +10,40 @@ def bubble_sort(unsorted_array)
   return puts "Wrong type of argument. It should be an array!" unless unsorted_array.kind_of? (Array)
 
   times_elts_were_swapped = 0
+  number_of_iterations_through_array = 0
 
   unsorted_array.each do |elt|
-
-    for i in 0...unsorted_array.length do
-
+    
+    i = 0
+    while i < unsorted_array.length - number_of_iterations_through_array
       spaceship_operator_result = unsorted_array[i] <=> unsorted_array[i + 1]
       unsorted_array[i], unsorted_array [i + spaceship_operator_result] = unsorted_array[i + spaceship_operator_result], unsorted_array[i] if spaceship_operator_result == 1
-      times_elts_were_swapped +=1 if spaceship_operator_result == 1
+      times_elts_were_swapped += 1 if spaceship_operator_result == 1
+
+      i += 1
+      puts "INSIDE WHILE: i is #{i}; spaceship_operator_result is #{spaceship_operator_result} and array is #{unsorted_array}"
 
     end
 
+    number_of_iterations_through_array += 1
     break if times_elts_were_swapped == 0
+    puts "OUTSIDE WHILE: i is #{i}; spaceship_operator_result is #{spaceship_operator_result} and array is #{unsorted_array}"
 
   end
 end
 
 def bubble_sort_by(arr)
-  i=0
+  i = 0
   times_elts_were_swapped = 0
-  if block_given?
-    arr.each do |el|
-      while i<arr.length-1
 
+  if block_given?
+
+    arr.each do |el|
+      while i < arr.length - (i + 1)
         comparison_result = yield(arr[i],arr[i+1])
-        arr[i], arr [i + 1] = arr[i + 1], arr[i] if comparison_result >0
-        times_elts_were_swapped +=1 if comparison_result > 0
-        i+=1
+        arr[i], arr [i + 1] = arr[i + 1], arr[i] if comparison_result > 0
+        times_elts_were_swapped += 1 if comparison_result > 0
+        i += 1
 
         puts "Inside while: i is: #{i}, time_elts_were_swapped is #{times_elts_were_swapped} and spaceship_operator_result is #{comparison_result}"
         puts "Inside while: arr is #{arr}"
@@ -45,15 +52,15 @@ def bubble_sort_by(arr)
         puts "Outside while: arr is #{arr}"
       break if times_elts_were_swapped == 0
     end
-    #puts arr
-  else
   end
 end
-#Expected result: ["hi","hey","hello","brando"]
+
+puts "====== Testing bubble_sort_by ========"
 puts bubble_sort_by(["hi","hello","hey","brando"]) {|el1,el2| el1.length - el2.length}
 
-=begin
- puts bubble_sort([6,5,4,3,2,1])
+
+puts "====== Testing bubble_sort ========"
+puts bubble_sort([6,5,4,3,2,1])
 puts "========"
 puts bubble_sort([3,5,4,0,3,2,1])
 puts "========"
@@ -61,11 +68,4 @@ puts bubble_sort([1,2,3,4,5,6])
 puts "========"
 puts bubble_sort(["m","c","f", "z","a","k"])
 puts "========"
-puts bubble_sort("random string") =end
-
-# puts "========"
-# [1,2,3,4,5,6].each do |left, right|
-#   puts "this is left #{left}"
-#   puts "this is right #{right}"
-# end
-=end
+puts bubble_sort("random string")
